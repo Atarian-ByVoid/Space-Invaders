2646088054bee0900eb9819a0a3924bdf27a5b67
Atarian <cgomespimentel7@gmail.com>
2023-02-23 14:09:14 -0300

    New Codes

---
+++ space-invaders/index.js
@@ -0,0 +1,70 @@
+import EnemyController from "./EnemyController.js";
+import Player from "./Player.js";
+import BulletController from "./BulletController.js";
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 1519;
+canvas.height = 600;
+
+const background = new Image();
+background.src = "images/spacePrime.jpg";
+
+const playerBulletController = new BulletController(canvas, 10, "blue", true);
+const enemyBulletController = new BulletController(canvas, 4, "red", false);
+const enemyController = new EnemyController(
+  canvas,
+  enemyBulletController,
+  playerBulletController
+);
+const player = new Player(canvas, 3, playerBulletController);
+
+let isGameOver = false;
+let didWin = false;
+
+function game() {
+  checkGameOver();
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  displayGameOver();
+  if (!isGameOver) {
+    enemyController.draw(ctx);
+    player.draw(ctx);
+    playerBulletController.draw(ctx);
+    enemyBulletController.draw(ctx);
+  }
+}
+
+function displayGameOver() {
+  if (isGameOver) {
+    
+    let text = didWin ? "You Win" : "Game Over";
+    let textOffset = didWin ? 2.5 : 3;
+  
+
+    ctx.fillStyle = "white";
+    ctx.font = "70px Magneto";
+    ctx.fillText(text, canvas.width / textOffset, canvas.height / 4);
+  }
+}
+
+function checkGameOver() {
+  if (isGameOver) {
+    return;
+  }
+
+  if (enemyBulletController.collideWith(player)) {
+    isGameOver = true;
+  }
+
+  if (enemyController.collideWith(player)) {
+    isGameOver = true;
+  }
+
+  if (enemyController.enemyRows.length === 0) {
+    didWin = true;
+    isGameOver = true;
+  }
+}
+
+setInterval(game, 1000 / 60);
